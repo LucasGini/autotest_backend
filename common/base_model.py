7ca71a58b2422edfbbac476fa538e6a341bd546a
lucasGini <dongqianglucas@gmail.com>
2023-11-15 21:10:17 +0800

    修复bug

--- common/base_model.py
+++ common/base_model.py
@@ -10,10 +10,10 @@ class BaseModel(models.Model):
         (1, '生效')
     )
     enable_flag = models.SmallIntegerField(verbose_name='有效标识', choices=ENABLE_CONST)
-    created_date = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     created_by = models.CharField(verbose_name='创建人', max_length=128)
-    updated_date = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    created_date = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     updated_by = models.CharField(verbose_name='更新人', max_length=128)
+    updated_date = models.DateTimeField(verbose_name='更新时间', auto_now=True)
 
     class Meta:
         abstract = True
